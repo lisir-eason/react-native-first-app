fd7d6f30b54690dbc0adb0d3e6dcfc0959df2cab
liqiang <695279538@qq.com>
2022-03-25 19:56:12 +0800

    Fisrt commit.

---
+++ TestComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const TestComponent = () => {
+  return (
+    <View>
+      <Text>test component</Text>
+    </View>
+  );
+};
+
+export default TestComponent;

84f77b9c9c0533d9c29e7ffb7ca7cb4487a51966
liqiang <695279538@qq.com>
2022-03-26 16:05:29 +0800

    Feat: add top navigator.

--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer} from 'react-navigation';
 
 const PopularPage = () => {
+  const TabNavigator = createAppContainer(
+    createMaterialTopTabNavigator({
+      PopularTab1: {
+        screen: PopularTab,
+        navigationOptions: {
+          title: 'Tab1',
+        },
+      },
+      PopularTab2: {
+        screen: PopularTab,
+        navigationOptions: {
+          title: 'Tab2',
+        },
+      },
+    }),
+  );
+
   return (
     <View style={styles.container}>
-      <Text>PopularPage</Text>
+      <TabNavigator />
+    </View>
+  );
+};
+
+const PopularTab = () => {
+  return (
+    <View>
+      <Text>PopularTab</Text>
     </View>
   );
 };
@@ -14,7 +41,6 @@ export default PopularPage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 30,
   },
 });
